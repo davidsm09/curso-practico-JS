94cf4a2a2f86235363e61f48a42bbb0c72bf0d19
davidcol <mezad55@gmail.com>
2022-07-09 01:24:53 -0500

    hicimos aplicacion de descuentos

--- Taller2/descuento.js
+++ Taller2/descuento.js
@@ -1,13 +1,23 @@
-const precioOriginal = 120;
-const descuento = 18;
-
-
 
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
     return precioConDescuento;
 }
+
+function clickPrecioFinal() {
+    const inputPrecio = document.getElementById("precio");
+    const precioValue = inputPrecio.value;
+
+    const inputDescuento = document.getElementById("descuento");
+    const descuentoValue = inputDescuento.value;
+
+    const precioFinal = calcularPrecioConDescuento(precioValue, descuentoValue);
+
+    const resultadoP = document.getElementById("mostrarPrecio");
+    resultadoP.innerText = "Tu precio es: $" + precioFinal;
+}
+
 
 
 /*console.log({
