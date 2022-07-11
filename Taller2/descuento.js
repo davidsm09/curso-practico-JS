baf752bbbbfe987cf5d1f799a92bfe991937cc94
davidcol <mezad55@gmail.com>
2022-07-10 23:52:55 -0500

    agregamos funcionalidad de cupon

--- Taller2/descuento.js
+++ Taller2/descuento.js
@@ -1,4 +1,4 @@
-
+//descuento Normal
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
@@ -18,6 +18,55 @@ function clickPrecioFinal() {
     resultadoP.innerText = "Tu precio es: $" + precioFinal;
 }
 
+
+//DEscuento con cupon
+
+function calcularPrecioConDescuentoCupon(precio, descuento, cupon) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+    const precioConDescuentoCupon = precioConDescuento - cupon;
+
+    if(precioConDescuentoCupon){
+        return (`Precio total con cupon es: $ ${precioConDescuentoCupon}.`);
+    }else {
+        return (`Error: Inserte un cupon válido.`);
+    } 
+}
+
+
+const cupons = ["codigo1", "codigo2", "codigo3"];
+
+function onclickButtonPriceCupon() {
+    const inputPrice1 = document.getElementById("precio");
+    const priceValue1 = inputPrice1.value;
+
+
+    const inputDiscount1 = document.getElementById("descuento");
+    const discountValue1 = inputDiscount1.value;
+
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+    
+    let descuento;
+
+    switch(couponValue) {
+        case "codigo1":
+            descuento = 10;
+        break;
+        case "codigo2":
+            descuento = 15;
+        break;
+        case "codigo3":
+            descuento = 30;
+        break;
+    }
+
+
+    const precioConDescuentoCupon = calcularPrecioConDescuentoCupon(priceValue1, discountValue1, descuento);
+
+    const ResultPC = document.getElementById("ResultPC");
+    ResultPC.innerText = precioConDescuentoCupon;
+}
 
 
 /*console.log({
