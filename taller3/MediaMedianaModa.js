f505420025022113d2facbf27045b56d4a8056f9
davidcol <mezad55@gmail.com>
2022-07-11 23:17:06 -0500

    iniciamos taller 3

---
+++ taller3/MediaMedianaModa.js
@@ -0,0 +1,28 @@
+//MEDIA
+
+function calcularMediaAritmetica(lista) {
+    //let sumaLista = 0;
+    //for (let i = 0; i < lista.length; i++) {
+    //     sumaLista = sumaLista + lista[i];
+    //}
+
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+
+    const promedioLista = sumaLista / lista.length;
+
+    return promedioLista;
+}
+
+
+//MEDIANA
+
+const lista1 = [
+    100,
+    200,
+    500,
+    40000000,
+];
