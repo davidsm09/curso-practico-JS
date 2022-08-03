665236df9d8f1a86e109ab3cedf58178183eb1a7
davidcol <mezad55@gmail.com>
2022-08-02 21:12:26 -0500

    terminamos curso

--- taller 4/analisis.js
+++ taller 4/analisis.js
@@ -1,6 +1,45 @@
+//Funciones de Ayuda
+
+function esPar(numero) {
+    return (numero % 2 ===0);
+        
+}
+
+function calcularMediaAritmetica(lista) {
+  
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+
+    const promedioLista = sumaLista / lista.length;
+    return promedioLista;
+}
+
+//Calculadora de mediana
+
+function medianaSalarios(lista) {
+    const mitad = parseInt(lista.length / 2);
+    
+    if (esPar(lista.length)) {
+        const personaMitad1 = lista[mitad - 1];
+        const personaMitad2 = lista[mitad];
+
+        const mediana = calcularMediaAritmetica([personaMitad1, personaMitad2]);
+        return mediana;
+    } else {
+        const personaMitad = lista[mitad];
+        return personaMitad;
+    }
+}
+
+
+//Mediana General
+
 const salariosCol = colombia.map(
     function (persona) {
-        return persona.salary;
+        return persona.salario;
     }
 );
 
@@ -10,18 +49,23 @@ const salariosColsorted = salariosCol.sort(
     }
 );
 
-function esPar(numero) {
-    return (numero % 2 ===0);
-        
-}
 
-function medianaSalariosCol(lista) {
-    const mitad = parseInt(lista.length / 2);
 
-    if (esPar(lista.length)) {
-        
-    } else {
-        const personaMitad = lista[mitad];
-        return personaMitad;
-    }
-}
+const medianaGeneralCol = medianaSalarios(salariosColsorted);
+
+//Mediana del Top 10%
+const spliceStart = (salariosColsorted.length * 90) / 100;
+const spliceCount = salariosColsorted.length - spliceStart;
+
+
+const salariosColTop10 = salariosColsorted.splice(
+    spliceStart,
+    spliceCount,
+);
+
+const medianaTop10Col = medianaSalarios(salariosColTop10);
+
+console.log({
+    medianaGeneralCol,
+    medianaTop10Col,
+});
--- taller 4/salarios.js
+++ taller 4/salarios.js
@@ -3,56 +3,56 @@ const colombia = [];
 colombia.push({
     nombre: "juanita",
     salario: 800,
-})
+});
 colombia.push({
     nombre: "david",
     salario: 500,
-})
+});
 colombia.push({
     nombre: "andres",
     salario: 1000,
-})
+});
 colombia.push({
     nombre: "felipe",
     salario: 700,
-})
+});
 colombia.push({
     nombre: "natalia",
     salario: 600,
-})
+});
 colombia.push({
     nombre: "andrea",
     salario: 400,
-})
+});
 colombia.push({
     nombre: "antonio",
     salario: 1500,
-})
+});
 colombia.push({
     nombre: "carla",
     salario: 1200,
-})
+});
 colombia.push({
     nombre: "juan",
     salario: 900,
-})
+});
 colombia.push({
     nombre: "raul",
     salario: 550,
-})
+});
 colombia.push({
     nombre: "marta",
     salario: 470,
-})
+});
 colombia.push({
     nombre: "marcela",
     salario: 600,
-})
+});
 colombia.push({
     nombre: "luz",
     salario: 600,
-})
+});
 colombia.push({
     nombre: "luis",
     salario: 740,
-})
+});
