9e9dce260a6a01ef17b46a30e488e73f9df6c7c0
davidcol <mezad55@gmail.com>
2022-08-10 21:42:46 -0500

    Dom 2

--- ManipulacionDom/DOM.js
+++ ManipulacionDom/DOM.js
@@ -1,1 +1,11 @@
-console.log("hello World");
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector('#inputid1');
+const input2 = document.querySelector('#inputid2');
+const btn = document.querySelector('#btnCalcular');
+const respuesta = document.querySelector('#respuesta');
+
+
+function btnOnClick() {
+    const suma = input1.value + input2.value;
+    respuesta.innerText = "tu suma es de:" + suma;
+}
