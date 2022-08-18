ccb5731094616b20c5a31d59e884c74bff25bfa8
davidcol <mezad55@gmail.com>
2022-08-17 21:24:39 -0500

    Dom 3 GitHub

--- ManipulacionDom/DOM.js
+++ ManipulacionDom/DOM.js
@@ -1,11 +1,15 @@
 const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
 const input1 = document.querySelector('#inputid1');
 const input2 = document.querySelector('#inputid2');
 const btn = document.querySelector('#btnCalcular');
 const respuesta = document.querySelector('#respuesta');
 
+form.addEventListener('submit', sumar);
 
-function btnOnClick() {
+function sumar(event) {
+    event.preventDefault();
     const suma = input1.value + input2.value;
     respuesta.innerText = "tu suma es de:" + suma;
-}
+} 
+
